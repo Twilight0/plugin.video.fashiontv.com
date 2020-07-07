9d1879aa98fbaa7c9313827409792bc15c042439
Twilight0 <twilight@freemail.gr>
2020-07-07 21:48:43 +0300

    Fashion TV 1.0.0

--- addon.py
+++ addon.py
@@ -14,12 +14,40 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+from __future__ import absolute_import
 
-import re
-from tulip import directory, client, control
+from sys import argv
+from tulip.compat import parse_qsl
+from resources.lib import fashiontv
 
-html = client.request('https://embed.cloudycdn.services/ftv/4dg_paris/?autoplay&player=hls')
+syshandle = int(argv[1])
+sysaddon = argv[0]
+params = dict(parse_qsl(argv[2].replace('?','')))
 
-stream = 'https:' + re.search(r"'(.+m3u8)'", html).group(1)
+########################################################################################################################
 
-directory.resolve(stream, meta={'title': 'Fashion TV Paris'}, icon=control.icon(), resolved_mode=False)
+action = params.get('action')
+url = params.get('url')
+
+########################################################################################################################
+
+if action is None:
+
+    fashiontv.Indexer().root()
+
+elif action == 'live':
+
+    fashiontv.Indexer().live()
+
+elif action == 'play':
+
+    fashiontv.Indexer().play(url)
+
+elif action == 'youtube':
+
+    fashiontv.Indexer().yt(url)
+
+elif action == 'cache_clear':
+
+    from tulip.cache import clear
+    clear(withyes=False)
--- resources/lib/fashiontv.py
+++ resources/lib/fashiontv.py
@@ -14,3 +14,212 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+from __future__ import absolute_import
+
+import json, re
+from os.path import exists as file_exists
+from tulip import directory, client, cache, control
+from tulip.parsers import itertags_wrapper
+from base64 import b64decode
+from zlib import decompress
+from youtube_registration import register_api_keys
+
+
+class Indexer:
+
+    def __init__(self):
+
+        self.list = []
+        self.main_link = 'https://www.fashiontv.com/'
+        self.fashion_tv_yt_channel = 'UCqzju-_WMKsgNx8R3QwupQQ'
+        self.ftv_yt_channel = 'UClnblsrZrugJfFs4ANPgNcA'
+        self.ftv_hot_yt_channel = 'UCDN6BTcfgHDuBQgCgXkU-cA'
+        self.ftv_parties_yt_channel = 'UCLiyW3PZFDSxIw6EES8MneQ'
+        self.ftv_asia_yt_channel = 'UCC2Iic6b-nhY7CSRUm_nyng'
+        self.scramble = (
+            'eJwVzNsKgjAAANBfkT2XTMW59SYSYmWgkdGTjDnmfeomMaN/Dz/gnC9oKnCyAPIQDBzi+xB5R8hmriBkNUHSHT1PuBBL'
+            '7NS+I4l2MLLpNClbSCl6viq+MDlqPmqbyQEcLECnpuy42dsw2ejDRGc0yyII1ix95Ui4Q1JsbabwR88zLa8m3ZXibOF6R'
+            '2/TB2HcVreCXkS8LU/Y5RG5b+D3Bwj/Nu0='
+        )
+
+        self.keys_registration()
+        self.check_inputstream_addon()
+
+    def root(self):
+
+        self.list = [
+            {
+                'title': control.lang(30001),
+                'action': 'live'
+            }
+            ,
+            {
+                'title': 'Fashion TV',
+                'action': 'youtube',
+                'image': 'https://yt3.ggpht.com/a/AATXAJxZonaXhogfs_ioQnbxlhR7NpepOecdrBYzbHmOm1k=s256',
+                'url': self.fashion_tv_yt_channel,
+                'isFolder': 'False', 'isPlayable': 'False'
+            }
+            ,
+            {
+                'title': 'FTV',
+                'action': 'youtube',
+                'image': 'https://yt3.ggpht.com/a/AATXAJxLtsDGDS6pB9XFt_2wIe3UrG9BU3UW7qVscEQgSg=s256',
+                'url': self.ftv_yt_channel,
+                'isFolder': 'False', 'isPlayable': 'False'
+            }
+            ,
+            {
+                'title': 'FTV HOT',
+                'action': 'youtube',
+                'image': 'https://yt3.ggpht.com/a/AATXAJxJIFyHhkMeUbmMEIahbXB8tOe5rQOHMIYjvFh21w=s256',
+                'url': self.ftv_hot_yt_channel,
+                'isFolder': 'False', 'isPlayable': 'False'
+            }
+            ,
+            {
+                'title': 'FashionTV Parties',
+                'action': 'youtube',
+                'image': 'https://yt3.ggpht.com/a/AATXAJwiPGuSb3438Ql1kh1Je6YLaWj52skL7MKHgaUJ=s256',
+                'url': self.ftv_parties_yt_channel,
+                'isFolder': 'False', 'isPlayable': 'False'
+            }
+            ,
+            {
+                'title': 'FashionTV Asia',
+                'action': 'youtube',
+                'image': 'https://yt3.ggpht.com/a/AATXAJxhBT9-z8FzvWbOZqbwzlKMaG9ib7NdRQVvhu3T=s256',
+                'url': self.ftv_parties_yt_channel,
+                'isFolder': 'False', 'isPlayable': 'False'
+            }
+        ]
+
+        plugin = 'plugin://plugin.video.youtube/channel/'
+
+        for item in self.list:
+
+            if 'url' in item:
+                item['url'] = ''.join([plugin, item['url'], '/?addon_id=', control.addonInfo('id')])
+
+            cache_clear = {'title': 30002, 'query': {'action': 'cache_clear'}}
+            item.update({'cm': [cache_clear]})
+
+        directory.add(self.list)
+
+    def list_live_items(self):
+
+        html = client.request(self.main_link)
+
+        items = itertags_wrapper(html, 'article', attrs={'id': 'stream-.+'})
+
+        for item in items:
+
+            title = itertags_wrapper(item.text, 'div', attrs={'class': 'a2a_kit a2a_kit_size_24 addtoany_list'}, ret='data-a2a-title')[0]
+            image = itertags_wrapper(item.text, 'img', attrs={'class': 'horizontal-thumbnail'}, ret='data-src')[0]
+            url = itertags_wrapper(item.text, 'a', attrs={'class': 'live-stream-button full-overlay'}, ret='data-source')[0]
+
+            data = {'title': title, 'image': image, 'url': url}
+
+            self.list.append(data)
+
+        return self.list
+
+    def live(self):
+
+        self.list = cache.get(self.list_live_items, 24)
+
+        if self.list is None:
+            return
+
+        for item in self.list:
+            item.update({'action': 'play', 'isFolder': 'False'})
+
+        directory.add(self.list, content='videos')
+
+    @staticmethod
+    def yt(url):
+
+        control.execute('Container.Update({},return)'.format(url))
+
+    @staticmethod
+    def resolve(url):
+
+        html = client.request(url)
+
+        stream = 'https:' + re.search(r"'(.+m3u8)'", html).group(1)
+
+        return stream
+
+    def play(self, url):
+
+        try:
+            addon_enabled = control.addon_details('inputstream.adaptive').get('enabled')
+        except KeyError:
+            addon_enabled = False
+
+        mimetype = None
+        manifest_type = None
+
+        if '.m3u8' in url:
+
+            manifest_type = 'hls'
+            mimetype = 'application/vnd.apple.mpegurl'
+
+        elif '.mpd' in url:
+
+            manifest_type = 'mpd'
+
+        directory.resolve(
+            self.resolve(url), dash=addon_enabled and ('.m3u8' in url or '.mpd' in url), mimetype=mimetype,
+            manifest_type=manifest_type
+        )
+
+    def keys_registration(self):
+
+        filepath = control.transPath(control.join(control.addon('plugin.video.youtube').getAddonInfo('profile'), 'api_keys.json'))
+
+        setting = control.addon('plugin.video.youtube').getSetting('youtube.allow.dev.keys') == 'true'
+
+        if file_exists(filepath):
+
+            f = open(filepath)
+    
+            jsonstore = json.load(f)
+
+            no_keys = control.addonInfo('id') not in jsonstore.get('keys', 'developer').get('developer')
+
+            if setting and no_keys: 
+    
+                keys = json.loads(decompress(b64decode(self.scramble)))
+    
+                register_api_keys(control.addonInfo('id'), keys['api_key'], keys['id'], keys['secret'])
+    
+            f.close()
+
+    @staticmethod
+    def check_inputstream_addon():
+
+        try:
+            addon_enabled = control.addon_details('inputstream.adaptive').get('enabled')
+        except KeyError:
+            addon_enabled = False
+
+        first_time_file = control.join(control.dataPath, 'first_time')
+
+        if not addon_enabled and not file_exists(first_time_file):
+
+            try:
+
+                yes = control.yesnoDialog(control.lang(30252))
+
+                if yes:
+
+                    control.enable_addon('inputstream.adaptive')
+                    control.infoDialog(control.lang(30402))
+
+                with open(first_time_file, 'a'):
+                    pass
+
+            except Exception:
+
+                pass
